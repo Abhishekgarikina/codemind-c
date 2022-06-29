7c411a44199ef8353fe77fc883799bcc691ba999
Abhishekgarikina <98166648+Abhishekgarikina@users.noreply.github.com>
2022-06-29 22:34:46 +0530

    #include<stdio.h> int main() {     int a[100],n,l,i;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     l=a[0];     for(i=0;i<n;i++)     {         if(l>a[i])         {             l=a[i];         }     }     printf("%d",l); }

---
+++ minimum_element_of_an_array.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int a[100],n,l,i;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    l=a[0];
+    for(i=0;i<n;i++)
+    {
+        if(l>a[i])
+        {
+            l=a[i];
+        }
+    }
+    printf("%d",l);
+}
