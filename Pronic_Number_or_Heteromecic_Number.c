84222c37c230e0ae3b7d61aae155babbc31f834c
Abhishekgarikina <98166648+Abhishekgarikina@users.noreply.github.com>
2022-06-29 14:02:50 +0530

    #include<stdio.h> #include<math.h> int main() {     int a=1,b=1,c1,c2,c,g,k,k2;     scanf("%d",&c);     c1=-b+sqrt(b*b-4*a*-c);     c2=-b-sqrt(b*b-4*a*-c);     g=2*a;     k=c1/g;     if(k*(k+1)==c) printf("YES");     else printf("NO");
    }

--- Pronic_Number_or_Heteromecic_Number.c
+++ Pronic_Number_or_Heteromecic_Number.c
@@ -1,22 +1,14 @@
 #include<stdio.h>
+#include<math.h>
 int main()
 {
-    int n,i,flag=0;
-    scanf("%d",&n);
-    for(i=0;i<n;i++)
-    {
-        if(i*(i+1)==n)
-        {
-            flag++;
-        }
-    }
-    if(flag!=0)
-    {
-        printf("YES");
-    }
-    else
-    {
-        printf("NO");
-    }
-    return 0;
+    int a=1,b=1,c1,c2,c,g,k,k2;
+    scanf("%d",&c);
+    c1=-b+sqrt(b*b-4*a*-c);
+    c2=-b-sqrt(b*b-4*a*-c);
+    g=2*a;
+    k=c1/g;
+    if(k*(k+1)==c) printf("YES");
+    else printf("NO");
+    
 }
