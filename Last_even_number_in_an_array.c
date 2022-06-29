9c73942177c03fa54511203ca4278c12492e1255
Abhishekgarikina <98166648+Abhishekgarikina@users.noreply.github.com>
2022-06-29 22:37:58 +0530

    #include<stdio.h> int main() {     int a[100],n,i,l;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         if(a[i]%2==0)         {             l=a[i];         }     }     printf("%d",l); }

---
+++ Last_even_number_in_an_array.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int a[100],n,i,l;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(a[i]%2==0)
+        {
+            l=a[i];
+        }
+    }
+    printf("%d",l);
+}
